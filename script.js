ce73254ae7ad1932c5eec911ea2b9dffda80361e
Anay Tripathi <tripathi.anay@gmail.com>
2023-11-12 22:46:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('signup-form');
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        var email = form.querySelector('input[type=email]').value;
+        
+        // Placeholder for what you would do with the email address
+        console.log('Email submitted: ' + email);
+        
+        // Here you would typically send the email to your server or a serverless function
+        // For now, we'll just clear the form and display a message
+        form.reset();
+        alert('Thank you for your interest! We will notify you when we launch.');
+    });
+});
 
